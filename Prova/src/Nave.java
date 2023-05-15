81bef243510c42ad6bec97d5806c217ef5427710
Pasquale <p.laterza10@studenti.uniba.it>
2023-05-15 23:29:05 +0200

    aggiungi nave.java

---
+++ Prova/src/Nave.java
@@ -0,0 +1,43 @@
+
+public class Nave {
+    private int dimensione;
+    private int celleRimaste;
+    private boolean affondata;
+    private Cella[] celle;
+
+
+    /**
+     * Default constructor
+     */
+    public Nave(int dimensione, int celleRimaste) {
+        this.dimensione = dimensione;
+        this.celleRimaste = dimensione;
+        this.affondata = false;
+        
+    }
+
+    public int getDimensione() {
+        return dimensione;
+    }
+    
+    public int getCelleRimaste() {
+        return celleRimaste;
+    }
+
+    public boolean isAffondata() {
+        return affondata;
+    }
+
+    public void setAffondata() {
+        affondata = true;
+    }
+
+    public void setCella(Cella[] celle) {
+        this.celle = celle;
+    }
+
+    public Cella[] getCella() {
+        return celle;
+    }
+
+}
