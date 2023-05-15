34bf67e6b7e79430f3c1ffb3394f131cfba995b2
Pasquale <p.laterza10@studenti.uniba.it>
2023-05-15 23:25:06 +0200

    aggiungi cella.java

---
+++ Prova/src/Cella.java
@@ -0,0 +1,53 @@
+
+public class Cella {
+    private int r;
+    private char c;
+    private Nave nave;
+    private boolean colpita;
+    private boolean occupata;
+
+
+    /**
+     * Default constructor
+     */
+    public Cella(int r, char c) {
+        this.r = r;
+        this.c = c;
+        nave = null;
+        colpita = false;
+        occupata = false;
+    }
+
+    public int getRiga() {
+        return r;
+    }
+
+    public char getColonna() {
+        return c;
+    }
+
+    public Nave getNave() {
+        return nave;
+    }
+
+    public boolean isColpita() {
+        return colpita;
+    }
+
+    public boolean isOccupata() {
+        return occupata;
+    }
+
+    public void setNave(Nave nave) {
+        this.nave = nave;
+    }
+
+    public void setColpita() {
+        colpita = true;
+    }
+
+    public void setOccupata() {
+        occupata = true;
+    }
+
+}
